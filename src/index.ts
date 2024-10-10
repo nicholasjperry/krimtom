5506673d935a948048ef0da9d9ba28b16a553820
Nick Perry <nick_perry@clinicalarchitecture.com>
2024-10-10 17:01:25 -0400

    POST method working; to figure out the timing of event creation...

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,63 @@ const client = new Client({
     intents: ["Guilds", "GuildMessages", "DirectMessages"],
 });
 
+// Server Id for <Inept>: 230431351172497418
+
+const apiKey = config.API_KEY;
+
+// #region Create Event
+
+// const createEvent = fetch('https://raid-helper.dev/api/v2/servers/230431351172497418/channels/1283125741944570021/event', {
+//     method: 'POST',
+//     headers: {
+//         'Authorization': `${apiKey}`,
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         leaderId: '346389429805383682',
+//         templateId: 5,
+//         title: 'Krimtom Make Test Event',
+
+//         description: 'Krimtom make test description.',
+//         advancedSettings: {
+//             image:  'https://preview.redd.it/unit-idea-felguard-v0-ypbjntiihmjd1.jpeg?width=640&crop=smart&auto=webp&s=11587e9d60f258bbafd2f99d3092b33a7fa8a48d',
+//         },
+//     })
+    
+// })
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data);
+//     return data;
+// });
+
+// #endregion
+
+// #region GET Server Event Data
+
+// const serverEvents = fetch('https://raid-helper.dev/api/v3/servers/230431351172497418/events', {
+//     method: 'GET',
+//     headers: {
+//         'Authorization': `${apiKey}`,
+//         'Content-Type': 'application/json'
+//     },
+// })
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data.postedEvents[0]);
+//     return JSON.stringify(data.postedEvents[0], null, 2);
+// });
+
+// const sendServerEventMessage = async() => {
+//     // Channel Id for #general: 230431351172497418
+//     const channel = client.channels.cache.get('230431351172497418') as TextChannel;
+//     channel?.send(await serverEvents);
+// }
+
+// #endregion
+
+// #region Krimtom's Intro Message
+
 // const sendIntroMessage = async () => {
     // const channel = client.channels.cache.get('1283122238626861097') as TextChannel;
     // channel?.send({
@@ -25,33 +82,11 @@ const client = new Client({
     // });
 // }
 
-// Server Id for <Inept>: 230431351172497418
-
-const apiKey = config.API_KEY;
-
-const serverEvents = fetch('https://raid-helper.dev/api/v3/servers/230431351172497418/events', {
-    method: 'GET',
-    headers: {
-        'Authorization': `${apiKey}`,
-        'Content-Type': 'application/json'
-    },
-})
-.then(response => response.json())
-.then(data => {
-    console.log(data.postedEvents[0]);
-    return JSON.stringify(data.postedEvents[0], null, 2);
-});
-
-const sendServerEventMessage = async() => {
-    // Channel Id for #general: 230431351172497418
-    const channel = client.channels.cache.get('230431351172497418') as TextChannel;
-    channel?.send(await serverEvents);
-}
+// #endregion
 
 client.login(config.DISCORD_TOKEN);
 
-client.on('ready', async (readyClient) => {
+client.once('ready', async (readyClient) => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-    // await serverEvents;
-    await sendServerEventMessage();
+    // await createEvent;
 });
